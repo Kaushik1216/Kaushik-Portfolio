6ce34930ac279b5f0555f1ba76af9c539cd72aa6
KaushikV <kaushik.vishwakarma2003@gmail.com>
2022-10-21 01:45:11 +0530

    dark light background change

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Contact from './components/Contact';
-import Downbar from './components/Downbar';
-import Skill from './components/Skill';
-import Project from './components/Project';
-import Home from './components/Home'
-import {Route,Routes} from 'react-router-dom';
-import {useState} from 'react';
-import About from './components/About';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import Downbar from "./components/Downbar";
+import Skill from "./components/Skill";
+import Project from "./components/Project";
+import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import About from "./components/About";
 function App() {
-  const[mode,setMode]=useState('light');
-  const themeChange=()=>{
-    if(mode==='light'){
-      setMode('dark');
-    }else{
-      setMode('light');
+  const [mode, setMode] = useState("light");
+  const [text,setText]=useState("light");
+  const[font,setFont]=useState('sun');
+  const themeChange = () => {
+    if (mode === "light") {
+      setMode("dark");
+      setFont("sun");
+      setText("light");
+      document.body.style.background="#29272A";
+    } else {
+      setMode("light");
+      setFont("moon");
+      setText("dark");
+      document.body.style.background="#ECECEC"
     }
-  }
+  };
   return (
-    <div className="App" id="app" >
-      
-     <Navbar themeChange={themeChange} />
-     <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/contact" element={<Contact/>}></Route>
-      <Route path="/skills" element={<Skill/>}></Route>
-      <Route path="/projects" element={<Project/>}></Route>
-     </Routes>
-     <Downbar/>
-    </div>
+      <div className="App" id="app">
+        <Navbar themeChange={themeChange} font={font} text={text} />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/skills" element={<Skill />}></Route>
+          <Route path="/projects" element={<Project />}></Route>
+        </Routes>
+        <Downbar />
+      </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import '../style/navbar.css'
 import { Link } from 'react-router-dom';
-import {useState} from 'react';
 export default function Navbar(props) {
-  const[font,setFont]=useState('sun');
-   const fontChange=()=>{
-     if(font==='sun'){
-      setFont('moon');
-     }else{
-      setFont('sun');
-     }
-   }
+  const navbar={
+  color:`${props.text}`}
   return (
     <>
     <nav className='menu-nav'>
       <div className="logo">
         <h2>Kaushik</h2>
       </div>
-      <div className="menu-link">
+      <div className="menu-link" style={navbar}>
         <ul>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/skills">Skills</Link></li>
@@ -28,7 +21,7 @@ export default function Navbar(props) {
       <div className="theme" >
       <input type="checkbox"  className="checkbox" id="checkbox" name="checkBox" onChange={props.themeChange}/><label htmlFor="checkbox" className="label">
   
-      <i className={`fa-solid fa-1x fa-${font}`} onClick={fontChange}></i>
+      <i className={`fa-solid fa-1x fa-${props.font}`} ></i>
   
       </label>
       </div>
