80a4a793ebc9fb6ce3df6c203aa766bd18ad4c76
KaushikV <kaushik.vishwakarma2003@gmail.com>
2022-10-23 18:00:56 +0530

    Skills page added

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,9 +1,58 @@
 import React from 'react'
-
+import '../style/skills.css'
+import { PythonlDiv ,CpplDiv,CplDiv} from '../datas/skiilsdata'
 export default function Skill() {
+  
   return (
    <>
-   
+   <div >
+    <div className="row">
+    <p className="pageinfo">Skills</p>
+    </div>
+    <div className="row">
+      <div className="code row">
+        <h3 class="title">Codding</h3>
+        <hr style={{color:"2px solid black"}}/>
+        <div className="row">
+          <span className='alignsub'><h5 className="subtitle">CP</h5>
+          <div className="star"><strong>1</strong><i class="fa-solid fa-star"></i></div><CplDiv className="starline" ></CplDiv><div className="star"><strong>5</strong><i class="fa-solid fa-star"></i></div></span>
+        </div>
+        <div className="row">
+          <span className='alignsub'><h5 className="subtitle">Python</h5>
+          <div className="star"><strong>1</strong><i class="fa-solid fa-star"></i></div><PythonlDiv className="starline" ></PythonlDiv><div className="star"><strong>5</strong><i class="fa-solid fa-star"></i></div></span>
+        </div>
+        <div className="row">
+          <span className='alignsub'><h5 className="subtitle">C++</h5>
+          <div className="star"><strong>1</strong><i class="fa-solid fa-star"></i></div><CpplDiv className="starline"></CpplDiv><div className="star"><strong>5</strong><i class="fa-solid fa-star"></i></div></span>
+        </div>
+      </div>
+      <div className="code row mt-5">
+        <h3 class="title">Full stack web development(MERN)</h3>
+        <hr style={{color:"2px solid black"}}/>
+        <div className="row">
+          <span className='alignsub'><h5 className="subtitle">Html</h5>
+          <div className="star"><strong>1</strong><i class="fa-solid fa-star"></i></div><CplDiv className="starline" ></CplDiv><div className="star"><strong>5</strong><i class="fa-solid fa-star"></i></div></span>
+        </div>
+        <div className="row">
+          <span className='alignsub'><h5 className="subtitle">Css</h5>
+          <div className="star"><strong>1</strong><i class="fa-solid fa-star"></i></div><CplDiv className="starline" ></CplDiv><div className="star"><strong>5</strong><i class="fa-solid fa-star"></i></div></span>
+        </div>
+        <div className="row">
+          <span className='alignsub'><h5 className="subtitle">JS</h5>
+          <div className="star"><strong>1</strong><i class="fa-solid fa-star"></i></div><CplDiv className="starline" ></CplDiv><div className="star"><strong>5</strong><i class="fa-solid fa-star"></i></div></span>
+        </div>
+       
+        <div className="row">
+          <span className='alignsub'><h5 className="subtitle">React Js</h5>
+          <div className="star"><strong>1</strong><i class="fa-solid fa-star"></i></div><PythonlDiv className="starline" ></PythonlDiv><div className="star"><strong>5</strong><i class="fa-solid fa-star"></i></div></span>
+        </div>
+        <div className="row">
+          <span className='alignsub'><h5 className="subtitle">Node Js</h5>
+          <div className="star"><strong>1</strong><i class="fa-solid fa-star"></i></div><CpplDiv className="starline"></CpplDiv><div className="star"><strong>5</strong><i class="fa-solid fa-star"></i></div></span>
+        </div>
+      </div>
+    </div>
+   </div>
    </>
   )
 }
---
+++ src/datas/skiilsdata.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+export const PythonlDiv = styled.div`
+  background: rgb(188,203,232);
+  background: linear-gradient(90deg, rgba(188,203,232,1) 20%, rgba(119,140,187,1) 33%, rgba(94,132,232,1) 72%, rgba(236,236,236,1) 72%);`
+
+export const CpplDiv=styled.div`
+background: rgb(188,203,232);
+background: linear-gradient(90deg, rgba(188,203,232,1) 20%, rgba(119,140,187,1) 33%, rgba(94,132,232,1) 61%, rgba(236,236,236,1) 61%);`
+
+
+export const CplDiv=styled.div`
+background: rgb(188,203,232);
+background: linear-gradient(90deg, rgba(188,203,232,1) 20%, rgba(119,140,187,1) 33%, rgba(94,132,232,1) 55%, rgba(236,236,236,1) 55%);
+`
