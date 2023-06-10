a32e551924bed511e0a38d8aa824a0a2975ca744
Kaushik1216 <kaushik.vishwakarma2003@gmail.com>
2023-06-10 17:59:29 +0530

    photo changed

--- src/components/About.js
+++ src/components/About.js
@@ -1,55 +1,138 @@
-import React from 'react'
-import '../style/about.css'
-import me from "./images/me3.png";
-import { useSelector } from 'react-redux';
+import React from "react";
+import "../style/about.css";
+import me from "./images/me5.jpeg";
+import { useSelector } from "react-redux";
 export default function About() {
-  const themecolor = useSelector((state)=>state.changeThetheme);
+  const themecolor = useSelector((state) => state.changeThetheme);
   return (
-   <>
-   <div id="about">
-   <p className='topic'style={{color:`${themecolor.headcolor}`}}>About Me</p>
-    <div className="row mx-auto">
-    <div className="col-md-6 col-push-12" id="smallimgdiv">
-                <div id="smallimg">
-                  <img src={me} alt="kaushik" />
+    <>
+      <div id="about">
+        <p className="topic" style={{ color: `${themecolor.headcolor}` }}>
+          About Me
+        </p>
+        <div className="row mx-auto">
+          <div className="col-md-6 col-push-12" id="smallimgdiv">
+            <div id="smallimg">
+            <figure class="image-container">
+              <div class="aspect-ratio-box">
+                <img
+                  src={me}
+                  alt="Kaushik"
+                  class="cropped-image"
+                  width="1920"
+                  height="1282"
+                  loading="lazy"
+                  style={{ borderColor: `${themecolor.headcolor}` }}
+                />
+              </div>
+            </figure>
+            </div>
+          </div>
+          <div className="col-md-6 col-12 leftabout">
+            <p
+              className="about-content"
+              style={{ color: `${themecolor.textcolor}` }}
+            >
+              Hi, my name is Kaushik Vishwakarma. I am currently pursuing B.Tech
+              in Computer Science and Engineering from IIT Mandi. I always enjoy
+              exploring and learning new technologies.
+              <br />
+              Currently, I have knowledge in DSA, web development, and machine
+              learning. I am particularly interested in contributing to
+              open-source projects, helping others, and taking on freelance
+              work. These pursuits allow me to not only apply my skills but also
+              contribute to the larger tech community and make a positive
+              impact.
+              <br /> Recently, I have developed several web development and
+              machine learning projects. I consider myself a quick learner and I
+              am always eager to tackle new challenges and expand my skill set.
+              <br />
+              <br />
+            </p>
+            <div className="row">
+              <p
+                className="about-content"
+                style={{ color: `${themecolor.textcolor}` }}
+              >
+                Here are some technology on which I worked recently :{" "}
+              </p>
+              <div className="row">
+                <div
+                  className="col-6"
+                  style={{ color: `${themecolor.textcolor}` }}
+                >
+                  <ul>
+                    <li>
+                      <i
+                        className="fa-solid fa-arrow-right"
+                        style={{ color: `${themecolor.headcolor}` }}
+                      ></i>
+                      C++
+                    </li>
+                    <li>
+                      <i
+                        className="fa-solid fa-arrow-right"
+                        style={{ color: `${themecolor.headcolor}` }}
+                      ></i>
+                      Python
+                    </li>
+                    <li>
+                      <i
+                        className="fa-solid fa-arrow-right"
+                        style={{ color: `${themecolor.headcolor}` }}
+                      ></i>
+                      React JS
+                    </li>
+                  </ul>
+                </div>
+                <div
+                  className="col-6"
+                  style={{ color: `${themecolor.textcolor}` }}
+                >
+                  <ul typeof="disk">
+                    <li>
+                      <i
+                        className="fa-solid fa-arrow-right"
+                        style={{ color: `${themecolor.headcolor}` }}
+                      ></i>
+                      Node JS
+                    </li>
+                    <li>
+                      <i
+                        className="fa-solid fa-arrow-right"
+                        style={{ color: `${themecolor.headcolor}` }}
+                      ></i>
+                      ML
+                    </li>
+                    <li>
+                      <i
+                        className="fa-solid fa-arrow-right"
+                        style={{ color: `${themecolor.headcolor}` }}
+                      ></i>
+                      Robotic's
+                    </li>
+                  </ul>
                 </div>
               </div>
-       <div className="col-md-6 col-12 leftabout">
-        <p className="about-content" style={{color:`${themecolor.textcolor}`}}>
-        Hi, My name is  Kaushik Vishwakarma .I am currently pursuing Btech in Computer Science        and Engineering from IIT MANDI.Always keep exploring and learning new technology.<br/>
-        I currently know DSA,Web development,Basic of Machine learning .Always Interested in contributing in open-source projects,helping people and Freeliance
-      <br/> I have recently developed many webdevelopment and machine-learning projects.I am a quick learner and am always eager to take on new challenges and expand my skill set. 
-      <br/>
-      <br/>
-      </p>
-      <div className="row">
-        <p className='about-content' style={{color:`${themecolor.textcolor}`}}>Here are some technology on which I worked recently : </p>
-        <div className="row">
-          <div className="col-6" style={{color:`${themecolor.textcolor}`}}>
-          <ul >
-          <li ><i className="fa-solid fa-arrow-right"style={{color:`${themecolor.headcolor}`}}></i>C++</li>
-          <li><i className="fa-solid fa-arrow-right"style={{color:`${themecolor.headcolor}`}}></i>Python</li>
-          <li><i className="fa-solid fa-arrow-right"style={{color:`${themecolor.headcolor}`}}></i>React JS</li>
-        </ul>
+            </div>
           </div>
-          <div className="col-6" style={{color:`${themecolor.textcolor}`}}>
-          <ul typeof='disk'>
-          <li><i className="fa-solid fa-arrow-right"style={{color:`${themecolor.headcolor}`}}></i>Node JS</li>
-          <li><i className="fa-solid fa-arrow-right"style={{color:`${themecolor.headcolor}`}}></i>ML</li>
-          <li><i className="fa-solid fa-arrow-right"style={{color:`${themecolor.headcolor}`}}></i>Robotic's</li>
-        </ul>
+          <div className="col-md-6 col-push-12" id="imgdiv">
+            <figure class="image-container">
+              <div class="aspect-ratio-box">
+                <img
+                  src={me}
+                  alt="Kaushik"
+                  class="cropped-image"
+                  width="1920"
+                  height="1282"
+                  loading="lazy"
+                  style={{ borderColor: `${themecolor.headcolor}` }}
+                />
+              </div>
+            </figure>
           </div>
         </div>
-        
       </div>
-             </div>
-             <div className="col-md-6 col-push-12" id="imgdiv">
-                <div id="img">
-                  <img src={me} alt="kaushik" />
-                </div>
-              </div>
-    </div>
-   </div>
-   </>
-  )
+    </>
+  );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,23 @@
 import React, { useEffect } from "react";
 import "../style/home.css";
-import { Twitter ,Facebook,Github,Linkdin,Twitterl,Facebookl,Githubl,Linkdinl} from "../datas/socialicon";
-import me from "./images/me1.png";
-import {html,coder,fulls} from "../datas/snipet"
-import hand from "./images/hi.gif"
+import {
+  Twitter,
+  Facebook,
+  Github,
+  Linkdin,
+  Twitterl,
+  Facebookl,
+  Githubl,
+  Linkdinl,
+} from "../datas/socialicon";
+import me from "./images/me5.jpeg";
+import { html, coder, fulls } from "../datas/snipet";
+import hand from "./images/hi.gif";
 import { useState } from "react";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 export default function Home() {
-  const themecolor = useSelector((state)=>state.changeThetheme);
+  const themecolor = useSelector((state) => state.changeThetheme);
 
   const item = {
     visible: {
@@ -17,8 +26,8 @@ export default function Home() {
       transition: {
         type: "spring",
         damping: 12,
-        stiffness: 200
-      }
+        stiffness: 200,
+      },
     },
     hidden: {
       opacity: 0,
@@ -26,114 +35,249 @@ export default function Home() {
       transition: {
         type: "spring",
         damping: 12,
-        stiffness: 200
-      }
-    }}
+        stiffness: 200,
+      },
+    },
+  };
 
-    const [code,setCode]=useState(html);
-    function codechange(x){
-      if(x==="Coder"){
+  const [code, setCode] = useState(html);
+  function codechange(x) {
+    if (x === "Coder") {
+      setCode(coder);
+    } else if (x === "fulls") {
+      setCode(fulls);
+    } else {
+      setCode(html);
+    }
+  }
+  function makeTimer() {
+    setInterval(() => {
+      let rand = Math.floor(Math.random() * 10) + 1;
+      if (rand >= 1 && rand <= 4) {
         setCode(coder);
+      } else if (rand <= 7) {
+        setCode(fulls);
+      } else {
+        setCode(html);
       }
-      else if(x==="fulls"){
-        setCode(fulls)
-      }
-      else{
-        setCode(html)
-      }
-    }
-    function  makeTimer(){
-      setInterval(() => {
-        let rand = Math.floor(Math.random() * 10) + 1
-         if(rand>=1 && rand<=4){
-          setCode(coder);
-         }else if( rand<=7){
-          setCode(fulls)
-         }else{
-           setCode(html);
-
-         }
-      }, 8000)
-    }
-    useEffect(()=>{
-      makeTimer();
-    })
+    }, 20000);
+  }
+  useEffect(() => {
+    makeTimer();
+  });
   return (
     <>
       <div className="container-fluid main_header">
         <div className="row">
           <div className=" col-12 mx-auto">
             <div className="row">
-              <div className="col-md-6 col-12 " id="kaushik" style={{color:`${themecolor.textcolor}`}}>
-               
+              <div
+                className="col-md-6 col-12 "
+                id="kaushik"
+                style={{ color: `${themecolor.textcolor}` }}
+              >
                 {/* <div></div> */}
                 <motion.div>
-                <div className="codeup text-center">{`${code.firstline}`}<br/>{`${code.second}`}<br id="break"/><br/>{`${code.middleone}`}</div>
-                  <motion.h5
-                  variants={item}
-                  initial="hidden"
-                  animate="visible">
-                    <p id="title">Hi<sup><img src={hand} style={{height:"40px"}}/></sup>I'm</p></motion.h5>
+                  <div className="codeup text-center">
+                    {`${code.firstline}`}
+                    <br />
+                    {`${code.second}`}
+                    <br id="break" />
+                    <br />
+                    {`${code.middleone}`}
+                  </div>
+                  <motion.h5 variants={item} initial="hidden" animate="visible">
+                    <p id="title">
+                      Hi
+                      <sup>
+                        <img src={hand} style={{ height: "40px" }} />
+                      </sup>
+                      I'm
+                    </p>
+                  </motion.h5>
 
                   <h1 id="name"> Kaushik Vishwakarma</h1>
-                  <h3 id="devinfo"style={{color:`${themecolor.headcolor}`}}><span onMouseEnter={()=>{codechange("Coder")}}>CODER</span>&nbsp;+&nbsp;<span onMouseEnter={()=>{codechange("fulls")}}>FULL STACK DEVELOPER</span></h3>
+                  <h3 id="devinfo" style={{ color: `${themecolor.headcolor}` }}>
+                    <span
+                      onMouseEnter={() => {
+                        codechange("Coder");
+                      }}
+                    >
+                      CODER
+                    </span>
+                    &nbsp;+&nbsp;
+                    <span
+                      onMouseEnter={() => {
+                        codechange("fulls");
+                      }}
+                    >
+                      FULL STACK DEVELOPER
+                    </span>
+                  </h3>
                   {/* <h6> 
                     A Enthusiastiac newbie in tech who everyday try to learn new tech stuf
                   </h6> */}
-                  <div className="row socialIcon text-start" style={{display:`${themecolor.textcolor==='#191919'?'':'none'}`}}>
-                    <motion.div className="col-1"
-                     initial={{transform:"scale(0)"}}
-                     animate={{scale:[0,1,1.5,1]}}
-                     transition={{type:'spring', duration:1, delay:0.25}}
-                     > <a target="_blank"rel="noreferrer" href="https://github.com/Kaushik1216"><Github></Github></a></motion.div>
-                    <motion.div className="col-1"
-                     initial={{transform:"scale(0)"}}
-                     animate={{scale:[0,1,1.5,1]}}
-                     transition={{type:'spring', duration:1, delay:0.5}}
-                     > <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/kaushik-vishwakarma-103b20223/"><Linkdin></Linkdin></a></motion.div>
-                    <motion.div className="col-1"
-                     initial={{transform:"scale(0)"}}
-                     animate={{scale:[0,1,1.5,1]}}
-                     transition={{type:'spring', duration:1, delay:0.75}}
-                     > <a rel="noreferrer" target="_blank" href="https://www.facebook.com/profile.php?id=100079232611563">
-                    <Facebook></Facebook></a></motion.div>
-                    <motion.div className="col-1"
-                     initial={{transform:"scale(0)"}}
-                     animate={{scale:[0,1,1.5,1]}}
-                     transition={{type:'spring', duration:1, delay:1}}
-                     ><a rel="noreferrer" target="_blank" href="https://twitter.com/Undefinekaushik"><Twitter></Twitter></a></motion.div>
-                   </div>
-                  <div className="row socialIcon text-start" style={{display:`${themecolor.textcolor==='#F8F8F8'?'':'none'}`}}>
-                    <motion.div className="col-1"
-                     initial={{transform:"scale(0)"}}
-                     animate={{scale:[0,1,1.5,1]}}
-                     transition={{type:'spring', duration:1, delay:0.25}}
-                     > <a target="_blank"rel="noreferrer" href="https://github.com/Kaushik1216"><Githubl></Githubl></a></motion.div>
-                    <motion.div className="col-1"
-                     initial={{transform:"scale(0)"}}
-                     animate={{scale:[0,1,1.5,1]}}
-                     transition={{type:'spring', duration:1, delay:0.5}}
-                     > <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/kaushik-vishwakarma-103b20223/"><Linkdinl></Linkdinl></a></motion.div>
-                    <motion.div className="col-1"
-                     initial={{transform:"scale(0)"}}
-                     animate={{scale:[0,1,1.5,1]}}
-                     transition={{type:'spring', duration:1, delay:0.75}}
-                     > <a rel="noreferrer" target="_blank" href="https://www.facebook.com/profile.php?id=100079232611563">
-                    <Facebookl></Facebookl></a></motion.div>
-                    <motion.div className="col-1"
-                     initial={{transform:"scale(0)"}}
-                     animate={{scale:[0,1,1.5,1]}}
-                     transition={{type:'spring', duration:1, delay:1}}
-                     ><a rel="noreferrer" target="_blank" href="https://twitter.com/Undefinekaushik"><Twitterl></Twitterl></a></motion.div>
-                   </div>
+                  <div
+                    className="row socialIcon text-start"
+                    style={{
+                      display: `${
+                        themecolor.textcolor === "#191919" ? "" : "none"
+                      }`,
+                    }}
+                  >
+                    <motion.div
+                      className="col-1"
+                      initial={{ transform: "scale(0)" }}
+                      animate={{ scale: [0, 1, 1.5, 1] }}
+                      transition={{ type: "spring", duration: 1, delay: 0.25 }}
+                    >
+                      {" "}
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://github.com/Kaushik1216"
+                      >
+                        <Github></Github>
+                      </a>
+                    </motion.div>
+                    <motion.div
+                      className="col-1"
+                      initial={{ transform: "scale(0)" }}
+                      animate={{ scale: [0, 1, 1.5, 1] }}
+                      transition={{ type: "spring", duration: 1, delay: 0.5 }}
+                    >
+                      {" "}
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://www.linkedin.com/in/kaushik-vishwakarma-103b20223/"
+                      >
+                        <Linkdin></Linkdin>
+                      </a>
+                    </motion.div>
+                    <motion.div
+                      className="col-1"
+                      initial={{ transform: "scale(0)" }}
+                      animate={{ scale: [0, 1, 1.5, 1] }}
+                      transition={{ type: "spring", duration: 1, delay: 0.75 }}
+                    >
+                      {" "}
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://www.facebook.com/profile.php?id=100079232611563"
+                      >
+                        <Facebook></Facebook>
+                      </a>
+                    </motion.div>
+                    <motion.div
+                      className="col-1"
+                      initial={{ transform: "scale(0)" }}
+                      animate={{ scale: [0, 1, 1.5, 1] }}
+                      transition={{ type: "spring", duration: 1, delay: 1 }}
+                    >
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://twitter.com/Undefinekaushik"
+                      >
+                        <Twitter></Twitter>
+                      </a>
+                    </motion.div>
+                  </div>
+                  <div
+                    className="row socialIcon text-start"
+                    style={{
+                      display: `${
+                        themecolor.textcolor === "#F8F8F8" ? "" : "none"
+                      }`,
+                    }}
+                  >
+                    <motion.div
+                      className="col-1"
+                      initial={{ transform: "scale(0)" }}
+                      animate={{ scale: [0, 1, 1.5, 1] }}
+                      transition={{ type: "spring", duration: 1, delay: 0.25 }}
+                    >
+                      {" "}
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://github.com/Kaushik1216"
+                      >
+                        <Githubl></Githubl>
+                      </a>
+                    </motion.div>
+                    <motion.div
+                      className="col-1"
+                      initial={{ transform: "scale(0)" }}
+                      animate={{ scale: [0, 1, 1.5, 1] }}
+                      transition={{ type: "spring", duration: 1, delay: 0.5 }}
+                    >
+                      {" "}
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://www.linkedin.com/in/kaushik-vishwakarma-103b20223/"
+                      >
+                        <Linkdinl></Linkdinl>
+                      </a>
+                    </motion.div>
+                    <motion.div
+                      className="col-1"
+                      initial={{ transform: "scale(0)" }}
+                      animate={{ scale: [0, 1, 1.5, 1] }}
+                      transition={{ type: "spring", duration: 1, delay: 0.75 }}
+                    >
+                      {" "}
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://www.facebook.com/profile.php?id=100079232611563"
+                      >
+                        <Facebookl></Facebookl>
+                      </a>
+                    </motion.div>
+                    <motion.div
+                      className="col-1"
+                      initial={{ transform: "scale(0)" }}
+                      animate={{ scale: [0, 1, 1.5, 1] }}
+                      transition={{ type: "spring", duration: 1, delay: 1 }}
+                    >
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://twitter.com/Undefinekaushik"
+                      >
+                        <Twitterl></Twitterl>
+                      </a>
+                    </motion.div>
+                  </div>
                 </motion.div>
 
-                <div className="codedown">{`${code.middlelast}`}<br id="break"/><br/>{`${code.lastsecond}`}<br/>{`${code.lastline}`}</div>
+                <div className="codedown">
+                  {`${code.middlelast}`}
+                  <br id="break" />
+                  <br />
+                  {`${code.lastsecond}`}
+                  <br />
+                  {`${code.lastline}`}
+                </div>
               </div>
               <div className="col-md-6 col-12" id="imgdiv">
-                <div id="img">
-                  <img src={me} alt="kaushik" />
-                </div>
+                <figure class="image-container">
+                  <div class="aspect-ratio-box" id="homeimg">
+                    <img
+                      src={me}
+                      alt="Kaushik"
+                      class="cropped-image"
+                      width="1920"
+                      height="1282"
+                      loading="lazy"
+                      style={{ borderColor: `${themecolor.headcolor}` }}
+                    />
+                  </div>
+                </figure>
               </div>
             </div>
           </div>
